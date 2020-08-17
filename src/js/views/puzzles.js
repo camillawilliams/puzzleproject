9ac00bcc5f0e3bc45d0c789ed2153792040bf99f
Carmi1234 <carmi.adame@hotmail.com>
2020-08-17 01:34:12 +0000

    cards now display in puzzles page

--- src/js/views/puzzles.js
+++ src/js/views/puzzles.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import { CardRow } from "../component/cards";
+import { Context } from "../store/appContext";
 
 export const Puzzles = props => {
+	const { store, actions } = useContext(Context);
 	return (
 		<div className="text-center mt-5">
-			<CardRow />
+			<CardRow cardData={store.puzzles} category="Category" />
 		</div>
 	);
 };
