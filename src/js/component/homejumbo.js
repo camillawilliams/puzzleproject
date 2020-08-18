736f69e5ab2ed371bd94c75e7ca185e1eadfe654
Carmi1234 <carmi.adame@hotmail.com>
2020-08-18 20:50:17 +0000

    added cart and color to home.js

--- src/js/component/homejumbo.js
+++ src/js/component/homejumbo.js
@@ -4,14 +4,12 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Jumbotron";
 
 export const HomeJumbo = () => (
-	<Jumbotron>
-		<Container className="h-100">
-			<h1 className="display-2">PUZZLED?</h1>
-			<h2 className="display-4">
-				SIGN IN FOR THE FUN TO BEGIN! &nbsp;
-				<i className="fas fa-puzzle-piece" />
-			</h2>
-			<p className="lead" />
-		</Container>
+	<Jumbotron className="jumbo-container">
+		<h1 className="display-2">PUZZLED?</h1>
+		<h2 className="display-4">
+			Sign in for the fun to begin! &nbsp;
+			<i className="fas fa-puzzle-piece" />
+		</h2>
+		<p className="lead" />
 	</Jumbotron>
 );
--- src/js/component/mynavbar.js
+++ src/js/component/mynavbar.js
@@ -10,8 +10,11 @@ import Button from "react-bootstrap/Button";
 export const MyNavbar = () => (
 	<div className="container">
 		<Navbar bg="light" expand="lg">
-			<Navbar.Brand href="#home">PUZZLE SWAP</Navbar.Brand>
 			<i className="fas fa-puzzle-piece" />
+			&nbsp;
+			<Navbar.Brand href="#home" className="font-weight-bold">
+				PUZZLE SWAP
+			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="mr-auto" />
@@ -19,14 +22,18 @@ export const MyNavbar = () => (
 					<FormControl type="text" placeholder="Search" className="mr-sm-2" />
 					<Button variant="outline-success">Search</Button>
 				</Form>
+
 				<NavDropdown title="Dropdown" id="basic-nav-dropdown">
 					<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
 					<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
 					<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
 					<NavDropdown.Divider />
-					<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+					<NavDropdown.Item href="#action/3.4">Go to Cart</NavDropdown.Item>
 				</NavDropdown>
 			</Navbar.Collapse>
+			<Nav.Link href="#link">
+				<i className="fas fa-shopping-cart" />
+			</Nav.Link>
 		</Navbar>
 	</div>
 );
