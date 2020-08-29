c282c5e1dc27b7c5c0aa8bd6549ea0a9a74f0022
athursto <alison.thurston@gmail.com>
2020-08-29 18:22:17 +0000

    adding reporting page

--- src/js/views/report.js
+++ src/js/views/report.js
@@ -4,22 +4,28 @@ import Button from "react-bootstrap/Button";
 
 export const Report = () => (
 	<Form className="login-form">
-		<h4>Sign in to your account</h4>
+		<h4>Report an Issue</h4>
+		<Form.Group controlId="formBasicIngo">
+			<Form.Label>Name</Form.Label>
+			<Form.Control type="text" placeholder="Your Name" />
+		</Form.Group>
 		<Form.Group controlId="formBasicEmail">
 			<Form.Label>Email address</Form.Label>
 			<Form.Control type="email" placeholder="Enter email" />
-			<Form.Text className="text-muted">We will never share your email with anyone else</Form.Text>
 		</Form.Group>
 
-		<Form.Group controlId="formBasicPassword">
-			<Form.Label>Password</Form.Label>
-			<Form.Control type="password" placeholder="Password" />
+		<Form.Group controlId="formPuzzleInfo">
+			<Form.Label>Order ID</Form.Label>
+			<Form.Control type="number" placeholder="ID Number" />
 		</Form.Group>
-		<Form.Group controlId="formBasicCheckbox">
-			<Form.Check type="checkbox" label="Remember me" />
+		<Form.Group controlId="issue">
+			<Form.Label>What&lsquo;s the Issue?</Form.Label>
+			<Form.Control as="textarea" rows="3">
+				Describe the issue
+			</Form.Control>
 		</Form.Group>
 		<Button variant="primary" type="submit">
-			Sign in
+			Report
 		</Button>
 	</Form>
 );
