61894e3f7f16f4a33da78e7ae73803a418f0a224
Carmi1234 <carmi.adame@hotmail.com>
2020-08-30 17:00:00 +0000

    nav changes

--- src/js/component/mynavbar.js
+++ src/js/component/mynavbar.js
@@ -13,7 +13,7 @@ export const MyNavbar = () => (
 			<i className="fas fa-puzzle-piece" />
 			&nbsp;
 			<Navbar.Brand href="#home" className="font-weight-bold">
-				PUZZLE SWAP
+				<Link to="/">PUZZLE SWAP</Link>
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
